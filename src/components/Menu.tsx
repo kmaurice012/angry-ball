b14012a8b0f119c3b7445a29d26645265e194891
kmaurice012 <kingmaurice012@gmail.com>
2023-05-04 16:03:28 +0300

    Working on the menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,26 @@
 import type { Component } from "solid-js";
-import { Stack } from "@suid/material";
+import { Button, ListItem, Stack } from "@suid/material";
 import { List, ListItemButton, Box, Typography } from "@suid/material";
-import { MenuList,MenuItem,ListItemIcon,ListItemText,Divider } from "@suid/material";
+import {
+  MenuList,
+  MenuItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+} from "@suid/material";
 
-import logo from "../logo.svg";
 import styles from "../App.module.css";
-import { Cloud, ContentCopy, ContentCut, ContentPaste } from "@suid/icons-material";
+import {
+  ArrowBack,
+  ArrowDownward,
+  ArrowLeftSharp,
+  ArrowRightSharp,
+  Cloud,
+  ContentCopy,
+  ContentCut,
+  ContentPaste,
+  Home,
+} from "@suid/icons-material";
 
 const App: Component = () => {
   return (
@@ -15,83 +30,105 @@ const App: Component = () => {
         height={550}
         bgcolor="rgba(255, 255, 255, 0.23)"
         borderRadius={"16px"}
+        my={5}
         sx={{
           boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
           backdropFilter: "blur(1.6px)",
         }}
         border={"1px solid rgba(255, 255, 255, 1)"}
       >
-        {/* <Stack>
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          height="100%"
+          width="100%"
+        >
           <Typography variant="h3" marginTop={4} gutterBottom>
             Game Menu
           </Typography>
-          <Stack textAlign="center" alignSelf="center">
-            <List component="nav">
-              <ListItemButton>MenuItem
-                <Typography variant="h5" marginTop={4} gutterBottom>
+
+          <nav aria-label="main mailbox folders">
+            <List
+              sx={{
+                width: 250,
+                "& .MuiButton-root": {
+                  transition: "all .5s",
+                  width: 250,
+                  border:'1px solid rgba(255, 255, 255, 0)',
+                  borderRadius: '5px',
+                  height: 50,
+                  "&:hover": {
+                    backgroundColor: "rgba( 255, 255, 255, 0.45 )",
+                    boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+                    backdropFilter: "blur(1.6px)",
+                    borderRadius: '16px',
+                    border:'1px solid rgba(255, 255, 255, 1)',
+                    color: "#b71d1d",
+
+                    "& .MuiButton-startIcon": {
+                      color: "#DD1544",
+                      opacity: 1,
+                    },
+                    "& .MuiButton-endIcon": {
+                      color: "#DD1544",
+                      opacity: 1,
+                    },
+                  },
+                },
+                "& .MuiButton-startIcon": {
+                  transition: "all .9s",
+                  opacity: 0,
+                },
+                
+                "& .MuiButton-endIcon": {
+                  transition: "all .9s",
+                  opacity: 0,
+                },
+              }}
+            >
+              <ListItem disablePadding>
+                <Button
+                  variant="text"
+                  sx={{ color: "white" }}
+                  startIcon={<ArrowRightSharp />}
+                  endIcon={<ArrowLeftSharp />}
+                >
                   New Game
-                </Typography>
-              </ListItemButton>
-              <ListItemButton>
-                <Typography variant="h5"  gutterBottom>
-                  Resume
-                </Typography>
-              </ListItemButton>
-              <ListItemButton>
-                <Typography variant="h5"  gutterBottom>
+                </Button>
+              </ListItem>
+              <ListItem disablePadding>
+                <Button
+                  variant="text"
+                  sx={{ color: "white" }}
+                  startIcon={<ArrowRightSharp />}
+                  endIcon={<ArrowLeftSharp />}
+                >
+                  Continue
+                </Button>
+              </ListItem>
+              <ListItem disablePadding>
+                <Button
+                  variant="text"
+                  sx={{ color: "white" }}
+                  startIcon={<ArrowRightSharp />}
+                  endIcon={<ArrowLeftSharp />}
+                >
                   Options
-                </Typography>
-              </ListItemButton> 
-              <ListItemButton>
-                <Typography variant="h5"  gutterBottom>
+                </Button>
+              </ListItem>
+              <ListItem disablePadding>
+                <Button
+                  variant="text"
+                  sx={{ color: "black" }}
+                  startIcon={<ArrowRightSharp />}
+                  endIcon={<ArrowLeftSharp />}
+                >
                   Quit
-                </Typography>
-              </ListItemButton>
-              
+                </Button>
+              </ListItem>
             </List>
-          </Stack>
-        </Stack> */}
-         <Stack>
-          <Typography variant="h3" marginTop={4} gutterBottom>
-            Game Menu
-          </Typography>
-          </Stack>
-          <MenuList>
-        <MenuItem>
-          <ListItemIcon>
-            <ContentCut fontSize="small" />
-          </ListItemIcon>
-          <ListItemText>Cut</ListItemText>
-          <Typography variant="body2" color="text.secondary">
-            ⌘X
-          </Typography>
-        </MenuItem>
-        <MenuItem>
-          <ListItemIcon>
-            <ContentCopy fontSize="small" />
-          </ListItemIcon>
-          <ListItemText>Copy</ListItemText>
-          <Typography variant="body2" color="text.secondary">
-            ⌘C
-          </Typography>
-        </MenuItem>
-        <MenuItem>
-          <ListItemIcon>
-            <ContentPaste fontSize="small" />
-          </ListItemIcon>
-          <ListItemText>Paste</ListItemText>
-          <Typography variant="body2" color="text.secondary">
-            ⌘V
-          </Typography>
-        </MenuItem>
-        <Divider />
-        <MenuItem>
-          <ListItemIcon>
-            <Cloud fontSize="small" />
-          </ListItemIcon>
-          <ListItemText>Web Clipboard</ListItemText>
-        </MenuItem>
-      </MenuList>
+          </nav>
+        </Stack>
       </Box>
     </Box>
   );
